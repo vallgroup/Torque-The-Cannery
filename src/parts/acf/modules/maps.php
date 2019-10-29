e48667ce92c2e4991d33934a5ff67e2f0a6af3a8
Al Notara <al@vallgroup.com>
2019-10-29 11:17:57 -0700

    Started styling the Maps page/slide

--- src/parts/acf/modules/maps.php
+++ src/parts/acf/modules/maps.php
@@ -1,9 +1,9 @@
 <?php
 
-$title   = get_sub_field( 'title' );
-$content = get_sub_field( 'content' );
-$cta     = get_sub_field( 'call_to_action' );
-$map = get_sub_field( 'map' );
+$title      = get_sub_field( 'title' );
+$content    = get_sub_field( 'content' );
+$cta        = get_sub_field( 'call_to_action' );
+$map        = get_sub_field( 'map' );
 
 $section_module = do_shortcode('[torque_map map_id="'.$map->ID.'"]');
 
