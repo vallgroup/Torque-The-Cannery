c579103fad1c155c7b280ddc4d4d4baef4171e75
Mario Vallejo <mario@vallgroup.com>
2019-11-06 17:35:31 -0600

    clear some QA tasks

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,58 @@
 import "../scss/main.scss";
 
-window.onload = () => {
-	takeOverBrowserScroll()
-	bindNavigation()
-
-	window.scrollTo({
-	  top: 0,
-	  left: 0,
-	  behavior: 'smooth'
-	});
-}
-
 let previousPos = 0;
 let __scrolling = false;
+let buffer = 100;
+let body = null;
+let sections = [];
 
-function takeOverBrowserScroll() {
+window.onload = init
 
-	const body = document.getElementsByTagName('body')[0]
-	const sections = document.getElementsByTagName('section')
+const TheCannery = {};
+
+function init() {
+
+	body = document.querySelector('body')
+	sections = document.querySelectorAll('.section')
+
+	fixSidebarContentHeight()
+
+	setInitialActiveSection()
+	takeOverBrowserScroll()
+	bindNavigation()
+}
+
+function fixSidebarContentHeight() {
+	const sidebars = document.querySelectorAll('.section-sidebar')
+	sidebars.forEach(sidebar => {
+		// get height for content (sidebar - logo)
+		const _logo = sidebar.querySelector('.section-sidebar-logo')
+		const _content = sidebar.querySelector('.section-sidebar-content')
+		// get the right height
+		const max = (+sidebar.offsetHeight - 100 /*account for 50px padding*/)
+		const logo = (+_logo.offsetHeight + 30 /*account for margin bottom*/)
+		const contentHeight = (max - logo)
+		// set the right height
+		_content.style.height = contentHeight
+	})
+}
+
+function setInitialActiveSection() {
 	const numberOfSections = sections.length || 0
-
-	const buffer = 100;
 
 	if (0 < numberOfSections) {
 
 		for (var i = 0; i < numberOfSections; i++) {
 			const __section = sections[i]
-			if (0 === i) __section.className += ' active'
+			if (0 === i) {
+				__section.classList.add('active')
+			}
 			__section.setAttribute('data-section-key', i)
 		}
 	}
+}
 
+function takeOverBrowserScroll() {
 
 	window.onscroll = (e) => {
 		const newPos = window.scrollY
@@ -38,18 +60,8 @@ function takeOverBrowserScroll() {
 		if (__scrolling) {
 			e.preventDefault();
 
-			let timeout = setTimeout(() => {
-
-				__scrolling = false
-				previousPos = window.scrollY
-
-			}, 400)
-
-
 			return false;
 		}
-
-
 
 		if ( newPos < (previousPos - buffer) ) {
 			// switch
@@ -57,56 +69,58 @@ function takeOverBrowserScroll() {
 			previousPos = newPos
 		}
 
-
 		if ( newPos > (previousPos + buffer) ) {
 			// switch
 			switchSection(1)
 			previousPos = newPos
 		}
 	}
+}
 
-	function switchSection(n) {
-		body.style.overflow = 'hidden'
-		n = n || 0
+function switchSection(n) {
+	n = n || 0
 
-		const currentSection = document.querySelector('.section.active')
-		const currentKey = +currentSection.getAttribute('data-section-key')
-		const newKey = (n + currentKey)
-		const nextSection = sections.item(newKey)
+	const currentSection = document.querySelector('.section.active')
+	const currentKey = +currentSection.getAttribute('data-section-key')
+	const newKey = (n + currentKey)
+	const nextSection = sections.item(newKey)
 
-		if (nextSection) {
-			currentSection.classList.remove('active')
-			nextSection.classList.add('active')
-			__scrolling = true;
+	if (nextSection) {
+		currentSection.classList.remove('active')
+		nextSection.classList.add('active')
 
-			window.scrollTo({
-			  top: nextSection.offsetTop,
-			  left: 0,
-			  behavior: 'smooth'
-			});
-		}
+		scrollToNextSection(nextSection)
+	}
+}
+
+function scrollToNextSection(nextSection) {
+	__scrolling = true;
+
+	body.style.overflow = 'hidden'
+
+	window.scrollTo({
+	  top: nextSection.offsetTop,
+	  left: 0,
+	  behavior: 'smooth'
+	});
+
+
+	setTimeout(() => {
+
 		body.style.overflow = ''
-	}
+		previousPos = window.scrollY
+		__scrolling = false
 
-	function getActiveElement() {
-		const sections = document.getElementsByTagName('section')
-		let _section = null;
-		for (var i = 0; i < sections.length; i++) {
-			const __section = sections[i]
-			if (-1 !== __section.className.indexOf('active')) {
-				_section = __section
-				return _section
-			}
-		}
-
-	}
+	}, 1000)
 }
 
 
 function bindNavigation() {
-	const navItems = document.querySelectorAll('.section-content-navigation .torque-menu-item-wrapper a')
+	const navItems = document.querySelectorAll('.section a')
 	navItems.forEach(navItem => {
-		if (navItem.href && navItem.href !== '') {
+		if (navItem.href
+			&& navItem.href !== ''
+			&& -1 !== navItem.href.indexOf('#')) {
 			const navLink = navItem.href.split('#')[1]
 
 			navItem.onclick = (e) => {
@@ -118,15 +132,8 @@ function bindNavigation() {
 				if (currentSection && nextSection && (nextSection !== currentSection)) {
 					currentSection.classList.remove('active')
 					nextSection.classList.add('active')
-					__scrolling = true;
-					window.scrollTo({
-					  top: nextSection.offsetTop,
-					  left: 0,
-					  behavior: 'smooth'
-					});
-					setTimeout(() => {
-						__scrolling = false
-					}, 1000)
+
+					scrollToNextSection(nextSection)
 				}
 			}
 		}
